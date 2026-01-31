f1fedfcee4ada72f31e7d84e12fe741227a0194a
Jeffrey Moniz <146683404+jeffonsicle@users.noreply.github.com>
2026-01-31 19:49:15 +0000

    added multiple functions and cleaned the code

--- A2/sched.c
+++ A2/sched.c
@@ -1,57 +1,119 @@
 #include <stdio.h>
 #include <string.h>
 
+char *PolicyCheck(char *argOne);
+
+char *RoundRobinIntegerCheck(char *argTwo);
+
+int FileCheck(char *argthree, char *argtwo, char *scheduleType, char *fileName);
+
+int FileReader(char *fileName, char *path);
+
+int N;
+
 int main(int argc, char *argv[]) 
 {
     char *scheduleType;
     char *quantumCheck;
-    int N;
-    char *fileName;
+    char fileName[256];
     char path[500];
 
-    //the checks below are to check if the first argument is asking to use Round Robin or First come first serve (clean this and add it to a seperate function called PolicyCheck)
-    if(strcmp(argv[1], "--policy=RR") == 0)
+    //the checks below are to check if the first argument is asking to use Round Robin or First come first serve
+    scheduleType = PolicyCheck(argv[1]);
+    if (scheduleType == NULL) 
     {
-        scheduleType = "Round Robin";
+        return 1;
     }
-    else if(strcmp(argv[1], "--policy=FCFS") == 0)
+
+    //this will check the second argument if the schedule type is Round Robin, it will ensure the integer is greater than 0.
+    if (strcmp(scheduleType, "Round Robin") == 0)
     {
-        scheduleType = "First come first serve";
+        quantumCheck = RoundRobinIntegerCheck(argv[2]);
+        if (quantumCheck == NULL) 
+        {
+            return 1;
+        }
+    }
+
+    //this code will check the schedule type and get the file name from the user based on the scheduler they are using
+    if(FileCheck(argv[3], argv[2], scheduleType, fileName) == 1)
+    {
+        return 1;
+    }
+
+    //this code will open the file, print the contents of the file, and close the file
+    /*sprintf(path, "/workspaces/CMPT_360_Assignment_2/A2/Workload_Samples/%s", &fileName);
+    FILE *fp = fopen(path, "r");
+    if (!fp) 
+    {
+        fprintf(stderr, "Error: Could not open file %s\n", path);
+        return 1;
+    }
+    char line[256];
+
+    while (fgets(line, sizeof(line), fp) != NULL) 
+    {
+        printf("%s", line);
+    }
+    fclose(fp);*/
+
+    if(FileReader(fileName, path) == 1)
+    {
+        return 1;
+    }
+
+    return 0;
+}
+
+char *PolicyCheck(char *argOne)
+{
+    //the checks below are to check if the first argument is asking to use Round Robin or First come first serve
+    if(strcmp(argOne, "--policy=RR") == 0)
+    {
+        return "Round Robin";
+    }
+    else if(strcmp(argOne, "--policy=FCFS") == 0)
+    {
+        return "First come first serve";
     }
     else
     {
         fprintf(stderr, "Error: invalid argument, use either --policy=RR OR --policy-FCFS\n");
-        return 1;
+        return NULL;
     }
+}
 
-    //this will check the second argument if the schedule type is Round Robin, it will ensure the integer is greater than 0. (clean this, add to a seperate function called RoundRobinIntegerCheck)
-    if (strcmp(scheduleType, "Round Robin") == 0)
-    {
-        if (sscanf(argv[2], "--quantum=%d", &N) == 1)
+char *RoundRobinIntegerCheck(char *argTwo)
+{
+    //this will check the second argument if the schedule type is Round Robin, it will ensure the integer is greater than 0.
+        if (sscanf(argTwo, "--quantum=%d", &N) == 1)
         {
             if (N > 0)
             {
-                quantumCheck = "True";
+                return "True";
             }
             else
             {
                 fprintf(stderr, "Error: Quantum must be greater than 0.\n");
-                return 1;
+                return NULL;
             }
         }
         else
         {
             fprintf(stderr, "Error: Invalid format for quantum. Use --quantum=N\n");
-            return 1;
+            return NULL;
         }
-    }
+    
+}
 
-    //this code will check the schedule type and get the file name from the user based on the scheduler they are using (to clean the code put this in a seperate function called FileCheck)
+int FileCheck(char *argthree, char *argtwo, char *scheduleType, char *fileName)
+{
+    //this code will check the schedule type and get the file name from the user based on the scheduler they are using
     if(strcmp(scheduleType, "First come first serve")==0)
     {
-        if (sscanf(argv[2], "--in=%s", &fileName) == 1)
+        if (sscanf(argtwo, "--in=%s", fileName) == 1)
         {
-            //continue with the program, if this was in another function just return the filename
+            return 0;
         }
         else
         {
@@ -61,9 +123,9 @@ int main(int argc, char *argv[])
     }
     else if(strcmp(scheduleType, "Round Robin")==0)
     {
-        if (sscanf(argv[3], "--in=%s", &fileName) == 1)
+        if (sscanf(argthree, "--in=%s", fileName) == 1)
         {
-            //continue with the program, if this was in another function just return the filename
+            return 0;
         }
         else
         {
@@ -71,17 +133,23 @@ int main(int argc, char *argv[])
             return 1;
         }
     }
+}
 
-    //this code will open the file and close the file
-    sprintf(path, "/workspaces/CMPT_360_Assignment_2/A2/Workload_Samples/%s", &fileName);
+int FileReader(char *fileName, char *path)
+{
+    sprintf(path, "/workspaces/CMPT_360_Assignment_2/A2/Workload_Samples/%s", fileName);
     FILE *fp = fopen(path, "r");
     if (!fp) 
     {
         fprintf(stderr, "Error: Could not open file %s\n", path);
         return 1;
     }
-    printf("Opened file: %s\n", path);
-    fclose(fp);
+    char line[256];
 
+    while (fgets(line, sizeof(line), fp) != NULL) 
+    {
+        printf("%s", line);
+    }
+    fclose(fp);
     return 0;
 }
